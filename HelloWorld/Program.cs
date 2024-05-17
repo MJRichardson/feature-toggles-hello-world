08c29a8c3fb8639b501e936e87f4f31b3d5a8558
Michael Richardson <michael.richardson@octopus.com>
2024-05-17 23:18:53 +1000

    added openfeature with octopus provider

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,15 @@
+using Octopus.OpenFeature.Provider;
+using OpenFeature;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add OpenFeature feature toggle client 
+var octopusFeatureTogglesClientId = builder.Configuration["FeatureToggles:ClientId"] ?? "";
+await OpenFeature.Api.Instance.SetProviderAsync(
+    new OctopusFeatureProvider(new OctopusFeatureConfiguration(octopusFeatureTogglesClientId)));
+builder.Services.AddScoped<IFeatureClient>(sp => OpenFeature.Api.Instance.GetClient());
+
+// Add MVC controllers 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
